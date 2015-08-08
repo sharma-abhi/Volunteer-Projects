fef98801764b2157357af18b231291d14ea43b07
sharma-abhi <asharma@ccs.neu.edu>
2015-08-08 16:19:15 -0400

    Initial exploration

---
+++ tulalens/model.py
@@ -0,0 +1,8 @@
+#!usr/bin/python
+__author__ = 'Abhijeet Sharma'
+
+import pandas
+import numpy as np
+import csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
